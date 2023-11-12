13d2715624611dbceebaa8d9dd86bc2dd87a5fe5
rahmanfajri <rahmanfajri.rf@gmail.com>
2023-11-12 18:51:19 +0700

    hitung emisi

--- app/src/main/java/com/jri/emisigas/maps/MapsActivity.kt
+++ app/src/main/java/com/jri/emisigas/maps/MapsActivity.kt
@@ -5,13 +5,13 @@ import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -43,6 +43,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationCallback: LocationCallback
     private var allLatLng = ArrayList<LatLng>()
     private var boundsBuilder = LatLngBounds.Builder()
+    private var totalDistance = 0.0
+
+    // Constants for idle estimation
+    private val IDLE_SPEED_THRESHOLD = 1.0 // km/jam
+    private val IDLE_SPEED_ESTIMATE = 5.0 // km/jam
+    private var idleStartTime: Long = 0
+    private var isIdleStarted = false
+
+    private var totalMovingEmission = 0.0
+    private var totalIdleEmission = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,9 +60,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -67,15 +75,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         createLocationRequest()
 
         binding.btnStart.setOnClickListener {
-            if(!isTracking){
+            if (!isTracking) {
                 clearMaps()
                 updateTrackingStatus(true)
                 startLocationUpdates()
-            }else{
+            } else {
                 updateTrackingStatus(false)
                 stopLocationUpdates()
+                val totalEmission = totalIdleEmission + totalMovingEmission
+                Log.d(TAG, "Total Emission: $totalEmission kg CO2")
+
+                // Kirim totalEmission ke ResultActivity
                 val intent = Intent(this@MapsActivity, ResultActivity::class.java)
+                intent.putExtra("TOTAL_EMISSION", totalEmission)
+                intent.putExtra("TOTAL_DISTANCE", totalDistance)
                 startActivity(intent)
+
+                // Selesaikan MapsActivity
+                finish()
             }
         }
 
@@ -104,19 +121,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
 
-    private fun checkPermission(permission: String): Boolean{
+    private fun checkPermission(permission: String): Boolean {
         return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun getMyLocation() {
         if (checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) &&
-            checkPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION))
-        {
-            fusedLocationClient.lastLocation.addOnSuccessListener{location: Location? ->
-                if(location != null){
+            checkPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) {
                     showStartMarker(location)
-                }else{
-                    Toast.makeText(this@MapsActivity, "Location is Not Found. Try Again!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(
+                        this@MapsActivity,
+                        "Location is Not Found. Try Again!",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         } else {
@@ -129,7 +150,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun showStartMarker(location: Location){
+    private fun showStartMarker(location: Location) {
         val startLocation = LatLng(location.latitude, location.longitude)
         mMap.addMarker(
             MarkerOptions()
@@ -139,15 +160,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 17f))
     }
 
-    private val resolutionLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()){ result ->
-        when(result.resultCode){
-            RESULT_OK -> Log.i(TAG, "onActivityResult: All location settings are satisfied.")
-            RESULT_CANCELED ->
-                Toast.makeText(this@MapsActivity, "You must Turn On GPS for Using This App!", Toast.LENGTH_SHORT).show()
+    private val resolutionLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> Log.i(TAG, "onActivityResult: All location settings are satisfied.")
+                RESULT_CANCELED ->
+                    Toast.makeText(
+                        this@MapsActivity,
+                        "You must Turn On GPS for Using This App!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+            }
         }
-    }
 
-    private fun createLocationRequest(){
+    private fun createLocationRequest() {
         locationRequest = LocationRequest.create().apply {
             interval = TimeUnit.SECONDS.toMillis(1)
             maxWaitTime = TimeUnit.SECONDS.toMillis(1)
@@ -161,31 +187,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .addOnSuccessListener {
                 getMyLocation()
             }
-            .addOnFailureListener { exception->
-                if(exception is ResolvableApiException){
+            .addOnFailureListener { exception ->
+                if (exception is ResolvableApiException) {
                     try {
                         resolutionLauncher.launch(IntentSenderRequest.Builder(exception.resolution).build())
-                    } catch (sendEx: IntentSender.SendIntentException){
+                    } catch (sendEx: IntentSender.SendIntentException) {
                         Toast.makeText(this@MapsActivity, sendEx.message, Toast.LENGTH_SHORT).show()
                     }
                 }
             }
     }
 
-    private fun updateTrackingStatus(newStatus: Boolean){
+    private fun updateTrackingStatus(newStatus: Boolean) {
         isTracking = newStatus
 
-        if (isTracking){
+        if (isTracking) {
             binding.btnStart.text = getString(R.string.stop_trip)
-        }else{
+        } else {
             binding.btnStart.text = getString(R.string.start_trip)
         }
     }
 
-    private fun createLocationCallback(){
-        locationCallback = object : LocationCallback(){
+    private fun createLocationCallback() {
+        locationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult) {
-                for (location in p0.locations){
+                for (location in p0.locations) {
                     Log.d(TAG, "onLocationResult: " + location.latitude + "," + location.longitude)
 
                     val lastLatLng = LatLng(location.latitude, location.longitude)
@@ -198,6 +224,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                             .addAll(allLatLng)
                     )
 
+                    // Hitung jarak tempuh dari titik sebelumnya
+                    if (allLatLng.size >= 2) {
+                        val distance = calculateDistance(allLatLng[allLatLng.size - 2], lastLatLng)
+                        totalDistance += distance
+                        Log.d(TAG, "onLocationResult: Distance - $distance, Total Distance - $totalDistance km")
+
+                        val movingEmission = distance * fuelEfficiency * fuelConsumption
+
+                        totalMovingEmission += movingEmission
+                        Log.d(TAG, "Moving Emission: $movingEmission kg CO2")
+
+                        // Hitung total emisi
+                        val totalEmission = totalMovingEmission
+                        Log.d(TAG, "Total Emission: $totalEmission kg CO2")
+                    }
+
+
+
                     boundsBuilder.include(lastLatLng)
                     val bounds: LatLngBounds = boundsBuilder.build()
                     mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 64))
@@ -206,21 +250,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun startLocationUpdates(){
+    private fun calculateIdleTime(location: Location): Long {
+        val isCurrentlyIdle = isIdle(location)
+
+        if (isCurrentlyIdle && !isIdleStarted) {
+            // Jika kendaraan baru saja menjadi idle, catat waktu mulai idle
+            idleStartTime = System.currentTimeMillis()
+            isIdleStarted = true
+        } else if (!isCurrentlyIdle && isIdleStarted) {
+            // Jika kendaraan telah bergerak setelah idle, reset waktu idle
+            idleStartTime = 0
+            isIdleStarted = false
+        }
+
+        // Hitung waktu idle dalam detik
+        val currentTime = System.currentTimeMillis()
+        val idleTimeInMillis = currentTime - idleStartTime
+        val idleTimeInSeconds = idleTimeInMillis / 1000
+
+        return if (isIdleStarted) {
+            idleTimeInSeconds
+        } else {
+            0
+        }
+    }
+
+    private fun isIdle(location: Location): Boolean {
+        // Implementasi sesuai dengan kebutuhan aplikasi Anda
+        // Misalnya, jika kecepatan kendaraan kurang dari batas tertentu
+        return location.speed < IDLE_SPEED_THRESHOLD
+    }
+
+    private fun calculateIdleDistance(idleTimeInSeconds: Long): Double {
+        // Implementasi sesuai dengan kebutuhan aplikasi Anda
+        // Misalnya, menggunakan estimasi kecepatan idle untuk menghitung jarak tempuh saat idle
+        val idleSpeed = IDLE_SPEED_ESTIMATE
+        return idleSpeed * idleTimeInSeconds.toDouble() / 3600.0 // Konversi ke kilometer
+    }
+
+
+    private fun startLocationUpdates() {
         try {
-            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
-        } catch (exception: SecurityException){
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } catch (exception: SecurityException) {
             Log.e(TAG, "Error: " + exception.message)
         }
     }
 
-    private fun stopLocationUpdates(){
+    private fun stopLocationUpdates() {
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
     override fun onResume() {
         super.onResume()
-        if(isTracking){
+        if (isTracking) {
             startLocationUpdates()
         }
     }
@@ -236,7 +323,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         boundsBuilder = LatLngBounds.Builder()
     }
 
+    private fun calculateDistance(startLatLng: LatLng, endLatLng: LatLng): Double {
+        val result = FloatArray(1)
+        Location.distanceBetween(
+            startLatLng.latitude, startLatLng.longitude,
+            endLatLng.latitude, endLatLng.longitude, result
+        )
+        return result[0].toDouble() / 1000.0 // Mengonversi meter menjadi kilometer
+    }
+
     companion object {
         private const val TAG = "MapsActivity"
+        private const val fuelEfficiency = 69300.0 // kg/TJ
+        private const val fuelConsumption = 4.95e-6 // TJ/km
     }
 }
--- app/src/main/java/com/jri/emisigas/result/ResultActivity.kt
+++ app/src/main/java/com/jri/emisigas/result/ResultActivity.kt
@@ -3,10 +3,19 @@ package com.jri.emisigas.result
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jri.emisigas.R
+import com.jri.emisigas.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val totalEmission = intent.getDoubleExtra("TOTAL_EMISSION", 0.0)
+        val totalDistance = intent.getDoubleExtra("TOTAL_DISTANCE", 0.0)
+
+        binding.totalEmission.text = "Total Emission: $totalEmission kg CO2"
+        binding.totalDistance.text = "Total Distance: $totalDistance km"
     }
 }
