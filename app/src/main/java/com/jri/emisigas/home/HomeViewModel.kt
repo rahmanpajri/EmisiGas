1bf69617e76f00a9403a18f91023bf3ee96cdf00
Rahman Pajri <rahmanfajri.rf@gmail.com>
2023-12-17 17:58:45 +0700

    rapi rapi

--- app/src/main/java/com/jri/emisigas/home/HomeViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.jri.emisigas.home
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class HomeViewModel : ViewModel() {
-
-}
